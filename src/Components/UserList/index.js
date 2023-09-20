f920ec2b28d819f19a0995ee8f59cfb531c1687b
AshutoshKapoor <67456685+AshutoshKapoor@users.noreply.github.com>
2023-09-20 17:26:01 +0530

    Minor change

--- src/Components/UserList/index.js
+++ src/Components/UserList/index.js
@@ -320,6 +320,7 @@ function UserList(props) {
                     cardContent={() => cardContent(user, index)}
                   />
                 );
+              else return <></>;
             })}
           </>
         )}
